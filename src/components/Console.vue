5e3e2eed4b92456f08caa8e5d8a734b223440984
Monira Khan <monirakhan21@gmail.com>
2019-01-23 08:32:46 -0500

    Fixed console code–put it all in console conponent
    This was a learning experience lol

--- src/components/Console.vue
+++ src/components/Console.vue
@@ -1,4 +1,19 @@
 //Define new console component
-Vue.component('console', {
+<template>
+  <div id="console">
+    <div style="height: 48px;"></div>
+  </div>
+</template>
 
-})
+<style>
+#console {
+  position: absolute;
+  width: 286px;
+  margin: 10px;
+  padding: 8px 8px;
+  background-color: rgb(225, 225, 225);
+  box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  border-radius: 3px;
+  z-index: 0;
+}
+</style>
